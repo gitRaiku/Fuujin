dedbc9151195d0fa9fa6bbb1fc55212593527a50
gitRaiku <rikunn.rikunn@gmail.com>
2025-02-25 11:53:01 +0200

    Add selection

--- Frontend/src/routes/playground/+page.svelte
+++ Frontend/src/routes/playground/+page.svelte
@@ -49,6 +49,11 @@
     cursor: pointer;
     transition: background 0.3s, border-color 0.3s, transform 0.2s;
   }
+  
+  :global(button.buttonSelected) {
+    color: black;
+    background: white;
+  }
 
   button:hover {
     background: #0056b3; /* Darker shade on hover */
@@ -69,13 +74,15 @@
     <div class="plcanvas"><canvas id="canvas"></canvas></div>
 
     <div id="controls" class="plbuttons">
-      <button id="red">Red Node</button>
-      <button id="blue">Blue Node</button>
-      <button id="green">Green Node</button>
-      <button id="yellow">Yellow Node</button>
+      <button id="node_addr" class="buttonSelected">Adder Node</button>
+      <button id="node_mult">Multiplier Node</button>
+      <button id="node_cnst">Constant Node</button>
+      <!--<button id="yellow">Yellow Node</button>
       <button id="purple">Purple Node</button>
       <button id="upload">Upload Audio</button>
-      <input type="file" id="audioFile" accept="audio/*" style="display: none;">
+      <input type="file" id="audioFile" accept="audio/*" style="display: none;">-->
+      <button id="node_audi">Audio Node</button>
+      <button id="node_ante">Antenna Node</button>
     </div>
   </div>
 </div>
@@ -84,14 +91,20 @@
 <script>
   import Header from "$lib/Header.svelte";
   import { onMount } from 'svelte';
-
   import { Playground } from './Playground.js';
   
   let pl;
-
   onMount(() => {
+    let nodeButtons = [
+      document.getElementById("node_addr"),
+      document.getElementById("node_mult"),
+      document.getElementById("node_cnst"),
+      document.getElementById("node_audi"),
+      document.getElementById("node_ante")
+    ]
+
     const canvas = document.getElementById("canvas");
-    pl = new Playground(canvas);
+    pl = new Playground(canvas, nodeButtons);
     pl.updateBounds(window.innerWidth, window.innerHeight);
 
     // canvas.addEventListener("click", (event) => {pl.onClick(event.offsetX, event.offsetY)})
@@ -99,8 +112,19 @@
     canvas.addEventListener("mousemove", (event) => {pl.mouseMove(event.offsetX, event.offsetY)})
     canvas.addEventListener("mouseup", (event) => {pl.mouseUp(event.offsetX, event.offsetY)})
 
+    for (const [i, node] of nodeButtons.entries()) {
+      node.addEventListener("click", () => {
+        for (const [li, lnode] of nodeButtons.entries()) {
+          if (li != i) { lnode.classList.remove("buttonSelected") }
+        }
+        node.classList.add("buttonSelected")
+        pl.nextNodeType = i
+      })
+    }
+
     window.addEventListener("resize", () => {pl.updateBounds(window.innerWidth, window.innerHeight); pl.drawGraph()})
 
+    /*
     document.getElementById("upload").addEventListener("click", () => {document.getElementById("audioFile").click()})
     document.getElementById("audioFile").addEventListener("change", (event) => {
       const file = event.target.files[0];
@@ -110,6 +134,7 @@
         console.log("Playing:", file.name);
       }
     });
+    */
     
   });
 </script>
--- Frontend/src/routes/playground/Playground.js
+++ Frontend/src/routes/playground/Playground.js
@@ -9,31 +9,208 @@ class RFElement {
   }
 
   update() {}
+  draw() {}
 }
 
-class AudioReader extends RFElement {
-  constructor(name) { super(name) }
+class RFAudio extends RFElement {
+  constructor(name, pos) { 
+    super(name); 
+    this.x = pos[0]
+    this.y = pos[1]
+    this.h = 100
+    this.w = 150
+    this.facets = [
+      {x: 50, y: 0, h: 30, w: 30, iout: 1},
+    ]
+  }
+
+  draw(pl, pos, size) {
+    let col;
+    if (this.focused == true) { col = "brown"; } else { col = "white"; }
+    pl.drawSquircle(pl, pos, size, 2, col)
+  }
+
 
   update() {
-
+    this.facets[0].val = 1.0;
   }
 }
 
-class DcOffset extends RFElement {
-  constructor(name, offset) { super(name); this.offset = offset }
+class RFAdder extends RFElement {
+  constructor(name, pos) { 
+    super(name); 
+    this.x = pos[0]
+    this.y = pos[1]
+    this.h = 100
+    this.w = 150
+    this.facets = [
+      {x: -75, y: 0, h: 30, w: 30, iout: 0},
+      {x: 0, y: -50, h: 30, w: 30, iout: 0},
+      {x: 75, y: 0, h: 30, w: 30, iout: 1},
+    ]
+  }
+
+  draw(pl, pos, size) {
+    let col;
+    if (this.focused == true) { col = "brown"; } else { col = "white"; }
+    pl.drawSquircle(pl, pos, size, 2, col)
+    pl.ctx.beginPath();
+    pl.ctx.strokeStyle = col;
+    pl.ctx.lineWidth = 2;
+    const ll = pl.downSz(10, 10);
+    const sl = pl.downSz(5, 10);
+    const cl = pl.downSz(30, 10);
+    pl.ctx.moveTo(pos[0] + sl[0] / 2        , pos[1] - sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] + sl[0] / 2 + ll[0], pos[1] - sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] + sl[0] / 2 + ll[0], pos[1] + sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] + sl[0] / 2        , pos[1] + sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] + sl[0] / 2        , pos[1] + sl[0] / 2 + ll[0])
+    pl.ctx.lineTo(pos[0] - sl[0] / 2        , pos[1] + sl[0] / 2 + ll[0])
+    pl.ctx.lineTo(pos[0] - sl[0] / 2        , pos[1] + sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] - sl[0] / 2 - ll[0], pos[1] + sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] - sl[0] / 2 - ll[0], pos[1] - sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] - sl[0] / 2        , pos[1] - sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] - sl[0] / 2        , pos[1] - sl[0] / 2 - ll[0])
+    pl.ctx.lineTo(pos[0] + sl[0] / 2        , pos[1] - sl[0] / 2 - ll[0])
+    pl.ctx.lineTo(pos[0] + sl[0] / 2        , pos[1] - sl[0] / 2        )
+    pl.ctx.stroke()
+    pl.ctx.beginPath();
+    pl.ctx.arc(pos[0], pos[1], cl[0], 0, 2 * Math.PI)
+    pl.ctx.stroke()
+  }
+
 
   update() {
-    this.output = this.input + this.offset
+    this.facets[2].val = this.facets[0].val + this.facets[1].val
   }
+}
+
+class RFMult extends RFElement {
+  constructor(name, pos) { 
+    super(name); 
+    this.x = pos[0]
+    this.y = pos[1]
+    this.h = 100
+    this.w = 100
+    this.facets = [
+      {x: -50, y: 0, h: 30, w: 30, iout: 0},
+      {x: 0, y: -50, h: 30, w: 30, iout: 0},
+      {x: 50, y: 0, h: 30, w: 30, iout: 1},
+    ]
+  }
+
+  draw(pl, pos, size) {
+    let col;
+    if (this.focused == true) { col = "brown"; } else { col = "white"; }
+    pl.drawSquircle(pl, pos, size, 2, col)
+    pl.ctx.beginPath();
+    pl.ctx.strokeStyle = col;
+    pl.ctx.lineWidth = 2;
+    const ll = pl.downSz(10, 10);
+    const sl = pl.downSz(5, 10);
+    const cl = pl.downSz(30, 10);
+    pl.ctx.moveTo(pos[0] + sl[0] / 2        , pos[1] - sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] + sl[0] / 2 + ll[0], pos[1] - sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] + sl[0] / 2 + ll[0], pos[1] + sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] + sl[0] / 2        , pos[1] + sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] + sl[0] / 2        , pos[1] + sl[0] / 2 + ll[0])
+    pl.ctx.lineTo(pos[0] - sl[0] / 2        , pos[1] - sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] - sl[0] / 2        , pos[1] - sl[0] / 2 - ll[0])
+    pl.ctx.lineTo(pos[0] + sl[0] / 2        , pos[1] - sl[0] / 2 - ll[0])
+    pl.ctx.lineTo(pos[0] + sl[0] / 2        , pos[1] - sl[0] / 2        )
+    pl.ctx.stroke()
+    pl.ctx.beginPath();
+    pl.ctx.arc(pos[0], pos[1], cl[0], 0, 2 * Math.PI)
+    pl.ctx.stroke()
+  }
+
+
+  update() {
+    this.facets[2].val = this.facets[0].val * this.facets[1].val
+  }
+}
+
+class RFConst extends RFElement {
+  constructor(name, pos) { 
+    super(name); 
+    this.x = pos[0]
+    this.y = pos[1]
+    this.h = 100
+    this.w = 100
+    this.facets = [
+      {x: 0, y: 50, h: 30, w: 30, iout: 1},
+    ]
+  }
+
+  draw(pl, pos, size) {
+    let col;
+    if (this.focused == true) { col = "brown"; } else { col = "white"; }
+    pl.drawSquircle(pl, pos, size, 2, col)
+    pl.ctx.beginPath();
+    pl.ctx.strokeStyle = col;
+    pl.ctx.lineWidth = 2;
+    const ll = pl.downSz(10, 10);
+    const sl = pl.downSz(5, 10);
+    const cl = pl.downSz(30, 10);
+    pl.ctx.moveTo(pos[0] + sl[0] / 2        , pos[1] - sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] + sl[0] / 2 + ll[0], pos[1] - sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] + sl[0] / 2 + ll[0], pos[1] + sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] + sl[0] / 2        , pos[1] + sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] + sl[0] / 2        , pos[1] - sl[0] / 2 - ll[0])
+    pl.ctx.lineTo(pos[0] + sl[0] / 2        , pos[1] - sl[0] / 2        )
+    pl.ctx.stroke()
+    pl.ctx.beginPath();
+    pl.ctx.arc(pos[0], pos[1], cl[0], 0, 2 * Math.PI)
+    pl.ctx.stroke()
+  }
+
+  update() {
+    this.facets[0].val = 1.0
+  }
+}
+
+class RFAntenna extends RFElement {
+  constructor(name, pos) { 
+    super(name); 
+    this.x = pos[0]
+    this.y = pos[1]
+    this.h = 100
+    this.w = 100
+    this.facets = [
+      {x: -50, y: 0, h: 30, w: 30, iout: 0},
+    ]
+  }
+
+  draw(pl, pos, size) {
+    pl.ctx.beginPath();
+    pl.ctx.lineWidth = 20;
+    pl.ctx.strokeStyle = "blue";
+    const ll = pl.downSz(10, 10);
+    const sl = pl.downSz(5, 10);
+    const cl = pl.downSz(30, 10);
+    pl.ctx.moveTo(pos[0] + sl[0] / 2        , pos[1] - sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] + sl[0] / 2 + ll[0], pos[1] - sl[0] / 2        )
+    pl.ctx.lineTo(pos[0] + sl[0] / 2 + ll[0], pos[1] + sl[0] / 2        )
+    pl.ctx.stroke()
+  }
+
+
+  update() {}
+}
+
+class ContextMenu {
+  
 }
 
 export class Playground {
-  constructor(canvas) {
+  constructor(canvas, nodeButtons) {
     this.canvas = canvas
     this.ctx = canvas.getContext("2d");
     this.nodes = []
     this.hovernode = null
     this.curaction = 0
+    this.nextNodeType = 0
+    this.nodeButtons = nodeButtons
   }
 
   updateBounds(w, h) {
@@ -70,18 +247,49 @@ export class Playground {
     return [cp[0] + cfp[0], cp[1] + cfp[1]]
   }
 
-  drawConnection(start, end) {
+  drawLine(c1, c2, width, style) {
     this.ctx.beginPath()
-    const c1 = this.facetCenter(start)
-    const c2 = this.facetCenter(end)
     this.ctx.moveTo(c1[0], c1[1])
     this.ctx.lineTo(c2[0], c2[1])
-    this.ctx.lineWidth = 3;
-    this.ctx.strokeStyle = "green"; 
-    this.ctx.stroke();
+    this.ctx.lineWidth = width
+    this.ctx.strokeStyle = style
+    this.ctx.stroke()
+  }
+
+  drawConnection(start, end) {
+    const c1 = this.facetCenter(start)
+    const c2 = this.facetCenter(end)
+    this.drawLine(c1, c2, 3, "green")
+  }
+
+  drawSquircle(pl, pos, size, width, col) {
+    pl.ctx.beginPath()
+    pl.ctx.lineWidth = width;
+    pl.ctx.strokeStyle = col;
+    const arcLen = pl.downSz(20, 20);
+    pl.ctx.arc(
+      pos[0] - size[0] / 2 + arcLen[0],
+      pos[1] - size[1] / 2 + arcLen[0],
+      arcLen[0], Math.PI * 2 / 2, Math.PI * 3 / 2, 0)
+    pl.ctx.arc(
+      pos[0] + size[0] / 2 - arcLen[0],
+      pos[1] - size[1] / 2 + arcLen[0],
+      arcLen[0], Math.PI * 3 / 2, Math.PI * 4 / 2, 0)
+    pl.ctx.arc(
+      pos[0] + size[0] / 2 - arcLen[0],
+      pos[1] + size[1] / 2 - arcLen[0],
+      arcLen[0], Math.PI * 4 / 2, Math.PI * 5 / 2, 0)
+    pl.ctx.arc(
+      pos[0] - size[0] / 2 + arcLen[0],
+      pos[1] + size[1] / 2 - arcLen[0],
+      arcLen[0], Math.PI * 5 / 2, Math.PI * 6 / 2, 0)
+    pl.ctx.moveTo(pos[0] - size[0] / 2            , pos[1] - size[1] / 2 + arcLen[0])
+    pl.ctx.lineTo(pos[0] - size[0] / 2            , pos[1] + size[1] / 2 - arcLen[0])
+    pl.ctx.stroke()
   }
 
   drawGraph() {
+    //console.log(`date ${new Date().getTime()}`)
     this.ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     let kms = canvas.getBoundingClientRect()
@@ -98,7 +306,11 @@ export class Playground {
       this.ctx.lineWidth = 1;
       const cp = this.upPose(node.x, node.y)
       const cz = this.downSz(node.w, node.h)
-      this.ctx.rect(cp[0] - cz[0] / 2, cp[1] - cz[0] / 2, cz[0], cz[1])
+      node.draw(this, cp, cz)
+      this.ctx.lineWidth = 1;
+      this.ctx.beginPath()
+      //this.ctx.drawImage(node.svg, cp[0] - cz[0] / 2, cp[1] - cz[0] / 2, cz[0], cz[1])
+      // this.ctx.rect(cp[0] - cz[0] / 2, cp[1] - cz[0] / 2, cz[0], cz[1])
       if (this.hovernode != null && this.hovernode[0] == ni && this.hovernode[1] == 0) {
         this.ctx.fillStyle = "green"; 
       } else {
@@ -116,10 +328,16 @@ export class Playground {
           cp[0] + cfp[0] - cfz[0] / 2, 
           cp[1] + cfp[1] - cfz[1] / 2, 
           cfz[0], cfz[1])
-      if (this.hovernode != null && this.hovernode[0] == ni && this.hovernode[1] == fi + 1) {
+        if (this.hovernode != null && this.hovernode[0] == ni && this.hovernode[1] == fi + 1) {
           this.ctx.fillStyle = "white"; 
+        } else if (this.hovernode2 != null && this.hovernode2[0] == ni && this.hovernode2[1] == fi + 1) {
+          this.ctx.fillStyle = "purple"; 
         } else {
-          this.ctx.fillStyle = "red"; 
+          if (this.nodes[ni].facets[fi].iout == 0) { 
+            this.ctx.fillStyle = "red"; 
+          } else {
+            this.ctx.fillStyle = "magenta"; 
+          }
         }
         this.ctx.fill();
         this.ctx.strokeStyle = "white"; this.ctx.stroke();
@@ -128,16 +346,30 @@ export class Playground {
   }
 
   addNode(f) {
-    this.nodes.push(
-      { 
-        x: f[0], y: f[1], // Pos
-        h: 100, w: 100,
-        facets: [ 
-          {x: -50, y: 0, h: 30, w: 100, iout: 0},
-          {x: 50, y: 50, h: 30, w: 100, iout: 1},
-          {x: 50, y: -50, h: 30, w: 100, iout: 1},
-        ]
-      })
+    switch (this.nextNodeType) {
+      case 0:
+        this.nodes.push(new RFAdder("Adder", f))
+        break
+      case 1:
+        this.nodes.push(new RFMult("Mult", f))
+        break
+      case 2:
+        this.nodes.push(new RFConst("Const", f))
+        break
+      case 3:
+        this.nodes.push(new RFAudio("Audio", f))
+        break
+      case 4:
+        this.nodes.push(new RFAntenna("Antenna", f))
+        break
+    }
+
+    if (this.nodeButtons != null) {
+      for (const [li, lnode] of this.nodeButtons.entries()) {
+        lnode.classList.remove("buttonSelected")
+      }
+    }
+    this.nextNodeType = 999
     this.drawGraph();
   }
 
@@ -166,40 +398,84 @@ export class Playground {
     return cnode
   }
 
+  clearFocus() {
+    this.nodes.forEach(node => {
+      node.focused = false
+    })
+  }
+
   mouseDown(x, y) {
+    this.clearFocus()
     const f = this.downPose(x, y)
     const cnode = this.getNodeAtPosition(f)
+    if (cnode != null && cnode[1] == 0) { this.nodes[cnode[0]].focused = true }
+    this.drawGraph()
     if (cnode != null && cnode[1] != 0) {
       this.curaction = 1
-      console.log(`Setting curnode ${cnode[0]} ${cnode[1]}`)
       this.curnode = cnode
     } else if (cnode != null && cnode[1] == 0) {
       this.curaction = 2
       this.curnode = cnode
     }
-    console.log(`Mdown`);
+  }
+
+  gnode(n) {
+    if (n == null || n[1] != 0) { return null }
+    return this.nodes[n[0]]
+  }
+
+  gfacet(n) {
+    if (n == null || n[1] == 0) { return null }
+    return this.nodes[n[0]].facets[n[1] - 1]
   }
 
   mouseUp(x, y) {
     const f = this.downPose(x, y)
     if (this.curaction == 0) {
-      this.addNode(f)
-    } else if (this.curaction == 1) {
-      const cnode = this.getNodeAtPosition(f)
-      if (this.curnode != null && this.curnode[1] != 0 && cnode != null && cnode[1] != 0) {
-        cnode[1] -= 1
-        if (this.nodes[this.curnode[0]].facets[this.curnode[1] - 1].iout == 0 && this.nodes[cnode[0]].facets[cnode[1]].iout == 1) {
-          console.log("01")
-          this.nodes[this.curnode[0]].facets[this.curnode[1] - 1].link = [cnode[0], cnode[1]]
-        } else if (this.nodes[this.curnode[0]].facets[this.curnode[1] - 1].iout == 1 && this.nodes[cnode[0]].facets[cnode[1]].iout == 0) {
-          console.log("10")
-          this.nodes[cnode[0]].facets[cnode[1]].link = [this.curnode[0], this.curnode[1] - 1]
+      if (this.nextNodeType != 999) {
+        this.addNode(f)
+      } else {
+        const cnode = this.gnode(this.getNodeAtPosition(f))
+        if (cnode != null) {
+          cnode.contextMenu = true
         }
       }
+    } else if (this.curaction == 1) {
+      const cnode = this.getNodeAtPosition(f)
+      this.hovernode2 = null
+      if (this.curnode != null && this.curnode[1] != 0 && cnode != null && cnode[1] != 0 && this.curnode[0] != cnode[0]) {
+        let inode = null, onode = null;
+        if (this.nodes[this.curnode[0]].facets[this.curnode[1] - 1].iout == 0 && this.nodes[cnode[0]].facets[cnode[1] - 1].iout == 1) {
+          inode = [this.curnode[0], this.curnode[1] - 1]
+          onode = [cnode[0], cnode[1] - 1]
+        } else if (this.nodes[this.curnode[0]].facets[this.curnode[1] - 1].iout == 1 && this.nodes[cnode[0]].facets[cnode[1] - 1].iout == 0) {
+          inode = [cnode[0], cnode[1] - 1]
+          onode = [this.curnode[0], this.curnode[1] - 1]
+        }
+
+        if (inode != null) {
+          const oldInLink = this.nodes[inode[0]].facets[inode[1]].link
+          const oldOutLink = this.nodes[onode[0]].facets[onode[1]].link
+          if (oldInLink != null) { 
+            if (this.nodes[oldInLink[0]].facets[oldInLink[1]].link == onode) {
+              this.nodes[inode[0]].facets[inode[1]].link = null 
+              this.nodes[onode[0]].facets[onode[1]].link = null
+              return
+            }
+            this.nodes[oldInLink[0]].facets[oldInLink[1]].link = null 
+          }
+          if (oldOutLink != null) {
+            this.nodes[oldOutLink[0]].facets[oldOutLink[1]].link = null 
+          }
+          this.nodes[onode[0]].facets[onode[1]].link = [inode[0], inode[1]] /// Output -> Input
+          this.nodes[inode[0]].facets[inode[1]].link = [onode[0], onode[1]] /// Input <- Output
+        }
+      }
+    } else if (this.curaction == 2) {
+      this.selnode = this.hovernode
     }
     this.curaction = 0
     this.drawGraph()
-    console.log(`Mup`);
   }
 
   mouseMove(x, y) {
@@ -209,11 +485,36 @@ export class Playground {
     if (this.curaction == 0) {
       this.hovernode = cnode;
     } else if (this.curaction == 1) {
-
+      this.hovernode2 = null;
+      if (this.curnode != null && this.curnode[1] != 0 && cnode != null && cnode[1] != 0 && this.curnode[0] != cnode[0]) {
+        if (this.nodes[this.curnode[0]].facets[this.curnode[1] - 1].iout == 0 && this.nodes[cnode[0]].facets[cnode[1] - 1].iout == 1) {
+          this.hovernode2 = cnode;
+        } else if (this.nodes[this.curnode[0]].facets[this.curnode[1] - 1].iout == 1 && this.nodes[cnode[0]].facets[cnode[1] - 1].iout == 0) {
+          this.hovernode2 = cnode;
+        }
+      }
     } else if (this.curaction == 2) {
-      this.nodes[this.curnode[0]].x = f[0]
-      this.nodes[this.curnode[0]].y = f[1]
+      if (
+        Math.abs(this.nodes[this.curnode[0]].x - f[0]) > 0.002 ||
+        Math.abs(this.nodes[this.curnode[0]].y - f[1]) > 0.002) {
+        this.curaction = 3;
+        this.nodes[this.curnode[0]].x = f[0]
+        this.nodes[this.curnode[0]].y = f[1]
+      }
+    } else if (this.curaction == 3) {
+        this.nodes[this.curnode[0]].x = f[0]
+        this.nodes[this.curnode[0]].y = f[1]
     }
-    if (onode != cnode) { this.drawGraph() }
+    if (
+      this.curaction != 0 || 
+      (onode != null && cnode != null && onode[0] != cnode[0] && onode[1] != cnode[1])) {
+      this.drawGraph() 
+    }
+
+    if (this.curaction == 1) {
+      const c1 = this.facetCenter([onode[0], onode[1] - 1])
+
+      this.drawLine(c1, [x, y])
+    }
   }
 }
